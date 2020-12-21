c21ec886e741d7985eb0a5a2b0fc0e96eb11871b
Zelihaars <72877610+Zelihaars@users.noreply.github.com>
2020-12-21 20:42:07 +0300

    Add files via upload

---
+++ dizi elemanlarını kendisi kadar yazdırma.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>
+	int main (){
+		int a[]={4,8,3,1,18,9,21,20,5,17};
+		int i,j,size;
+		size=10;
+		for(i=0;i<size;i++){
+			for(j=0;j<a[i];j++){
+				printf("%d",a[i]);		
+					}
+					printf("\n");
+		}
+		getch();
+	}
